c5f4ea4f6895d65d3b27cabdfe11c98517b686b1
Briana Eng <brianaeng@gmail.com>
2016-12-13 13:53:35 -0800

    Added more tests and functions for checkWinner & makeMove.

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -5,8 +5,11 @@ describe('Game', function(){
   var gameTest = new Game();
 
   it('should initialize a board', function(){
-
     expect(gameTest.board).toEqual([0,0,0,0,0,0,0,0,0]);
+  });
+
+  it('should initialize a variable for number of moves', function(){
+    expect(gameTest.totalMoves).toEqual(0);
   });
 
   it('should initialize two players', function(){
@@ -17,9 +20,39 @@ describe('Game', function(){
   describe('setFirstPlayer', function(){
     it('should randomly choose first player', function(){
       expect(gameTest.setFirstPlayer).toBeDefined();
-    })
-  })
+    });
+  });
 
+  describe('makeMove', function(){
+    it('should allow player one to choose a position and add move to board', function(){
+      expect(gameTest.makeMove(0)).toEqual([1,0,0,0,0,0,0,0,0]);
+      expect(gameTest.totalMoves).toEqual(1);
+    });
+
+    it('should allow player two to choose a position and add move to board', function(){
+      expect(gameTest.makeMove(1)).toEqual([1,-1,0,0,0,0,0,0,0]);
+      expect(gameTest.totalMoves).toEqual(2);
+    });
+
+    it('should allow player one to choose a position and add move to board', function(){
+      expect(gameTest.makeMove(7)).toEqual([1,-1,0,0,0,0,0,1,0]);
+      expect(gameTest.totalMoves).toEqual(3);
+    });
+
+    it('should not allow player two to choose a position that is taken', function(){
+      expect(function() {
+        gameTest.makeMove(0);
+      }).toThrow("This spot is already taken.");
+    });
+  });
+
+  describe('checkWinner', function(){
+    it('should check to see if a player won', function(){
+      var winningGame = new Game();
+      winningGame.board = [1,1,1,0,0,0,0,0,0];
+      expect(winningGame.checkWinner()).toEqual("Player One wins!");
+    });
+  });
 
   // describe()
 
--- src/game.js
+++ src/game.js
@@ -1,15 +1,63 @@
-
 var Game = function(){
   this.board = [0,0,0,0,0,0,0,0,0];
-  this.playerOne = 'Player One';
-  this.playerTwo = 'Player Two';
-
+  this.playerOne = 'Player One'; //X & 1
+  this.playerTwo = 'Player Two'; //O & -1
+  this.totalMoves = 0;
 };
+
+// Game.prototype.setFirstPlayer = function(){
+//   var players = [this.playerOne, this.playerTwo];
+//   return players[Math.floor(Math.random()*2)];
+// };
 
 Game.prototype.setFirstPlayer = function(){
-  var players = [this.playerOne, this.playerTwo]
-  return players[Math.floor(Math.random()*2)];
+  var coinSides = ["HEADS", "TAILS"];
+  return coinSides[Math.floor(Math.random()*2)] + "is Player One and you are X. Player Two is O.";
 };
+
+Game.prototype.checkWinner = function(){
+   var winningMoves = [
+    [this.board[0], this.board[1], this.board[2]],
+    [this.board[3], this.board[4], this.board[5]],
+    [this.board[6], this.board[7], this.board[8]],
+    [this.board[0], this.board[3], this.board[6]],
+    [this.board[1], this.board[4], this.board[7]],
+    [this.board[2], this.board[5], this.board[8]],
+    [this.board[0], this.board[4], this.board[8]],
+    [this.board[6], this.board[4], this.board[2]]
+  ];
+
+  for (var i = 0; i < winningMoves.length; i++) {
+    var sum = winningMoves[i].reduce(function(a, b) {
+      return a + b;
+    }, 0);
+
+    if (sum === 3) {
+      return "Player One wins!";
+    }
+    else if (sum === -3) {
+      return "Player Two wins!";
+    }
+  }
+};
+
+Game.prototype.makeMove = function(location){
+  if (this.board[location] !== 0){
+    throw "This spot is already taken.";
+  }
+  else {
+    this.totalMoves += 1;
+    if (this.totalMoves % 2 !== 0){
+      this.board[location] = 1;
+    } //When move is odd (Player 1)
+    else {
+      this.board[location] = -1;
+    } //When move is even (Player 2)
+  }
+  this.checkWinner();
+  return this.board;
+};
+
 
 
 
