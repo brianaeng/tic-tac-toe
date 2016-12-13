7903a0c9dc1c1a5379995f51b3c0c6d07e9fe330
Brandi Phillips <milligan.brandi@gmail.com>
2016-12-13 11:29:52 -0800

    tests are passing hurray

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,13 +1,25 @@
 import Game from "game";
-// import Board from "game";
+
 
 describe('Game', function(){
-
+  var gameTest = new Game();
 
   it('should initialize a board', function(){
-    var gameTest = new Game();
-    expect(gameTest.name).toEqual("One")
+
+    expect(gameTest.board).toEqual([0,0,0,0,0,0,0,0,0]);
   });
+
+  it('should initialize two players', function(){
+    expect(gameTest.playerOne).toEqual('Player One');
+    expect(gameTest.playerTwo).toEqual('Player Two');
+  });
+
+  describe('setFirstPlayer', function(){
+    it('should randomly choose first player', function(){
+      expect(gameTest.setFirstPlayer).toBeDefined();
+    })
+  })
+
 
   // describe()
 
--- src/game.js
+++ src/game.js
@@ -1,12 +1,16 @@
-var Board = function(){};
 
 var Game = function(){
-  this.board = new Board();
-  this.name = "One";
+  this.board = [0,0,0,0,0,0,0,0,0];
+  this.playerOne = 'Player One';
+  this.playerTwo = 'Player Two';
 
+};
+
+Game.prototype.setFirstPlayer = function(){
+  var players = [this.playerOne, this.playerTwo]
+  return players[Math.floor(Math.random()*2)];
 };
 
 
 
 export default Game; // this is how it is exported with webpack
-//module.exports = Board;
